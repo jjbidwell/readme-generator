67935f4c4af4533dbdf666e95c69c78c9cf66e05
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-30 16:20:33 -0700

    added questions that the user is to answer

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 var fs = require("fs");
 var inquirer = require('inquirer');
 
+console.log('Welcome to the README generator. To create a formatted README file to include in your GitHub Project, please answer the following prompts:')
+
+
 inquirer
     .prompt([
         {
             type: "input",
-            message: "Is the app working?",
-            name: "test"
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Write a brief description of our project.",
+            name: "desription"
+        },
+        {
+            type: "number",
+            message: "How many steps does a user have to take to install your project?",
+            name: "installationSteps"
+
+        },
+        {
+            type: "input",
+            message: "Write steps on how a user installs your project, making sure to separate each step with a period.",
+            name: "installationInstructions"
+        },
+        {
+            type: "input",
+            message: "Provide instrutions for use, making sure to separate each step with a period.",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub username",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Enter your email",
+            name: "Email"
         },
         {
             type: "list",
-            message: "How cool is this super-sweet app?",
-            name: "Cool",
+            message: "What license does your project use?",
+            name: "license",
             choices: [
-                "Incredibly modern and cool",
-                "Only a little modern and cool",
-                "Complete garbage"
+                "MIT",
+                "GNU GPLv3",
+                "GNU LGPLv3",
+                "GNU AGPLv3",
+                "Apache",
+                "ISC",
+                "Boost Software",
+                "The Unlicense",
+                "Mozilla Public License"
             ]
         }
     ])
