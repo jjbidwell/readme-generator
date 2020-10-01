598f9c966d01dd9f66440091cc6d9ebcfd95d006
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-01 16:15:40 -0700

    Created a break statement to deal with badge licenses. They now appear properly at the top of the page

--- generated-readme.md
+++ generated-readme.md
@@ -1,8 +1,8 @@
-#  
-[![License: GNU AGPLv3](https://img.shields.io/badge/License-GNU%20AGPLv3-red.svg)](https://opensource.org/licenses/GNU-AGPLv3)  
+# Title 
+[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)  
 
 ## Description
-
+asdlidasi;aadhgio ojasdghidhagihoi oilh agiolhasdgiol hiosdg 
 
 ## Table of Contents
 1. [Title](#Title)
@@ -15,24 +15,43 @@
 
 
 ## Installation
+1. jadhgasihioasdg
+2. asdg
+3. asd
+4. sdg
+5. adg
+6. sdg
+7. sdg
 
 
 ## Usage Directions
+1. adsghfhbykyi6
+2. dqddtgrjikjtshghujhgfghjgfd
+3. dfhjkjhgfghjhgfghjhgfd
+4. asdfjghfgdf
 
 
 ## License 
-This app uses GNU AGPLv3
+This app uses Attribution 4.0 International
 
 ## Contributing 
 Here is how you can contribute: 
+* ashgreyu5vasrcrv  erg
+* yjyt
   
   
 ## Testing  
+1. rag567h77
+2. 5t44
+3. 5
+4. 7
+5. tr
+6. f
 
 
 ## Questions
-GitHub Profile: [](https://github.com/)  
-Email:   
+GitHub Profile: [jjbidwell262@gmail.com](https://github.com/jjbidwell262@gmail.com)  
+Email: jjbidwell262@gmail.com  
 
 Please feel free to contact me via email or through my GitHub profile if you have any questions.
 
--- index.js
+++ index.js
@@ -88,15 +88,19 @@ inquirer
             message: "What license does your project use?",
             name: "license",
             choices: [
-                "MIT",
-                "GNU GPLv3",
-                "GNU LGPLv3",
-                "GNU AGPLv3",
                 "Apache 2.0",
-                "ISC",
-                "Boost 1.0",
-                "The Unlicense",
-                "Mozilla Public License",
+                "Boost Software Licence 1.0",
+                'BSD 3-Clause License',
+                'CC0',
+                'Attribution 4.0 International',
+                'Eclipse Public License 1.0',
+                'GNU GPL v3',
+                'GNU AGPL v3',
+                'GNU LGPL v3',
+                'GNU FDL v1.3',
+                'IBM Public License Version 1.0',
+                'MIT',
+                'Mozilla Public License 2.0'
             ]
         }
     ])
@@ -116,9 +120,48 @@ inquirer
         makeList(usageArray, usage, "usage");
         makeList(contributionArray, contributions, "contribute");
         makeList(testArray, test, "testing");
-        licenseBadge = `[![License: ${license}](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-red.svg)](https://opensource.org/licenses/${license.replace(" ", "-")})`
 
-
+        switch(license){
+            case "Apache 2.0":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";        
+            break;
+            case "Boost Software Licence 1.0":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";        
+            break;
+            case "BSD 3-Clause License":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";        
+            break;                 
+            case "CC0":
+            licenseBadge = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";        
+            break;
+            case "Attribution 4.0 International":
+            licenseBadge = "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)";        
+            break;
+            case "Eclipse Public License 1.0":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";        
+            break;
+            case "GNU GPL v3":
+            licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0";        
+            break;
+            case "GNU AGPL v3":
+            licenseBadge = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";        
+            break;
+            case "GNU LGPL v3":
+            licenseBadge = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";        
+            break;                 
+            case "GNU FDL v1.3":
+            licenseBadge = "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";        
+            break;
+            case "IBM Public License Version 1.0":
+            licenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";        
+            break;
+            case "MIT":
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";        
+            break;
+            case "Mozilla Public License 2.0":
+            licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";        
+            break;
+            }
 let newReadMe = 
 `# ${title} 
 ${licenseBadge}  
